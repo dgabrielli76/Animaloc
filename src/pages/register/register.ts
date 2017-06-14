7f127306f567c6c7bf8243c0fa4e9b6676d9d466
Yoann Perro <yoann.perro@gmail.com>
2017-06-14 14:06:12 +0200

    Complete register page

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the RegisterPage page.
@@ -14,11 +15,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RegisterPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RegisterPage');
+  createAccount() {
+    this.navCtrl.push(HomePage);
   }
-
 }
