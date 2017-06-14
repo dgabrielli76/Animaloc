a3158362ffc164c9f3378edfec0818f64c9008fa
Yoann Perro <yoann.perro@gmail.com>
2017-06-14 11:24:39 +0200

    Fix marker bug on Android

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {
  GoogleMaps,
@@ -19,9 +19,9 @@ export class HomePage {
 
   }
 
-  ngAfterViewInit() {
-   this.loadMap();
-  };
+  ionViewDidLoad() {
+    this.loadMap();
+  }
 
   loadMap() {
     let element: HTMLElement = document.getElementById('map');
@@ -32,33 +32,29 @@ export class HomePage {
     // You must wait for this event to fire before adding something to the map or modifying it in anyway
     map.one(GoogleMapsEvent.MAP_READY).then(
      () => {
-       console.log('Map is ready!');
-       // Now you can add elements to the map like the marker
+       // create LatLng object
+       let ionic: LatLng = new LatLng(43.0741904,-89.3809802);
+
+       // create CameraPosition
+       let position: CameraPosition = {
+        target: ionic,
+        zoom: 18,
+        tilt: 30
+       };
+
+       // move the map's camera to position
+       map.moveCamera(position);
+
+       // create new marker
+       let markerOptions: MarkerOptions = {
+        position: ionic,
+        title: 'Ionic'
+       };
+
+       map.addMarker(markerOptions).then((marker: Marker) => {
+         marker.showInfoWindow();
+       });
      }
     );
-
-    // create LatLng object
-    let ionic: LatLng = new LatLng(43.0741904,-89.3809802);
-
-    // create CameraPosition
-    let position: CameraPosition = {
-     target: ionic,
-     zoom: 18,
-     tilt: 30
-    };
-
-    // move the map's camera to position
-    map.moveCamera(position);
-
-    // create new marker
-    let markerOptions: MarkerOptions = {
-     position: ionic,
-     title: 'Ionic'
-    };
-
-    const marker = map.addMarker(markerOptions)
-     .then((marker: Marker) => {
-        marker.showInfoWindow();
-      });
-    }
+  }
 }
